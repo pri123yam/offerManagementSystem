fe9c84343958cd18b9dfba5b598c5ffb99703706
Priyam Guha <pri_1234@Priyams-MacBook-Air.local>
2020-02-20 10:48:57 +0530

    data storage service and css

---
+++ src/app/all_Services/create-offer-data.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { EventEmitter } from 'protractor';
+import { Lease } from '../model/Lease/lease';
+import { Model_Geo } from '../model/Model-Geo/mod_geo';
+import { Tactic } from '../model/Tactic/tactic';
+import { STactic } from '../model/Tactic/select_tactic';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CreateOfferDataService {
+  private selectedBrandGeoData = new Model_Geo();
+  private leaseData = new Lease();
+  private tacticData = new STactic;
+  setBrandGeoData(val : any){
+    this.selectedBrandGeoData = val;
+  }
+  getBrandGeoData(){
+    return this.selectedBrandGeoData;
+  }
+  getLeaseData(){
+    return this.leaseData;
+  }
+  setLeaseData(val : any){
+    this.leaseData = val;
+  }
+  setTacticData(val : any){
+    this.tacticData = val;
+  }
+  getTacticData(){
+    return this.tacticData;
+  }
+}
